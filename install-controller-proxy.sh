af1f02f71cdb3f3f59c8b24f9575288b7569ee4f
Michael Hill <michael@mrhconsultantsinc.com>
2025-07-18 10:08:21 -0400

    210: Indent

--- install-controller-proxy.sh
+++ install-controller-proxy.sh
@@ -4,7 +4,7 @@
 # This script installs and configures the JSON proxy service
 
 # ChillXand Controller Version - Update this for each deployment
-CHILLXAND_VERSION="v1.0.209"
+CHILLXAND_VERSION="v1.0.210"
 
 set -e  # Exit on any error
 
@@ -592,8 +592,7 @@ class ReadOnlyHandler(http.server.BaseHTTPRequestHandler):
                 self._save_update_state(update_state)
                 
                 # Create update script
-# Create update script using .format() to avoid f-string dollar sign issues
-update_script = '''#!/bin/bash
+                update_script = '''#!/bin/bash
 set -e
 sleep 2
 
