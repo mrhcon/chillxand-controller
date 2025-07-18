2f62a0d12f31d65f6af507f3ed93df50ea41c292
Michael Hill <michael@mrhconsultantsinc.com>
2025-07-18 08:28:17 -0400

    200

--- install-controller-proxy.sh
+++ install-controller-proxy.sh
@@ -4,7 +4,7 @@
 # This script installs and configures the JSON proxy service
 
 # ChillXand Controller Version - Update this for each deployment
-CHILLXAND_VERSION="v1.0.200"
+CHILLXAND_VERSION="v1.0.201"
 
 set -e  # Exit on any error
 
