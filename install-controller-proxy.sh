67588f8f720fb6276ce1f85a0901e316b14de603
Michael Hill <michael@mrhconsultantsinc.com>
2025-07-18 15:35:54 -0400

    247: Updates to stats

--- install-controller-proxy.sh
+++ install-controller-proxy.sh
@@ -4,7 +4,7 @@
 # This script installs and configures the JSON proxy service
 
 # ChillXand Controller Version - Update this for each deployment
-CHILLXAND_VERSION="v1.0.246"
+CHILLXAND_VERSION="v1.0.247"
 
 # Atlas API Configuration
 ATLAS_API_URL="http://atlas.devnet.xandeum.com:3000/api/pods"
