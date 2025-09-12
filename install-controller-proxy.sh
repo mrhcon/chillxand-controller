6387be91d12ad5bdf3ee825681b077b4dd8c0736
mrhcon <michael@mrhconsultantsinc.com>
2025-09-11 21:16:40 -0400

    Reversing out the change to seenodes

--- install-controller-proxy.sh
+++ install-controller-proxy.sh
@@ -4,7 +4,7 @@
 # This script installs and configures the JSON proxy service
 
 # ChillXand Controller Version - Update this for each deployment
-CHILLXAND_VERSION="v1.1.16"
+CHILLXAND_VERSION="v1.1.15"
 
 # Atlas API Configuration
 ATLAS_API_URL="http://atlas.devnet.xandeum.com:3000/api/pods"
